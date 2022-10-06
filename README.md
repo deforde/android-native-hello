73904ea3258850a11b991c572c275a1649fb1fbb
deforde <daniel.forde001@gmail.com>
2022-10-06 11:41:08 +0200

    tweak scripts, add readme

---
+++ README.md
@@ -0,0 +1,21 @@
+# android-native-hello
+
+This repo provides a simple, automated and self-contained example demonstrating how to compile a purely native executable for Android and run it in an emulator.
+
+## Install
+To install the Android cmd line tools, platform, emulator and NDK, run:
+```
+./install.sh
+```
+
+## Build
+To build the native application, run:
+```
+./build.sh
+```
+
+## Run
+To run the emulator, push the native app, invoke it and terminate, run:
+```
+./run.sh
+```
--- install.sh
+++ install.sh
@@ -6,12 +6,18 @@ SCRIPT_DIR=$(realpath ${0%/*})
 SDK_DIR=$SCRIPT_DIR/sdk
 mkdir -p $SDK_DIR
 
-curl -LO https://dl.google.com/android/repository/commandlinetools-linux-8512546_latest.zip
-unzip commandlinetools-linux-8512546_latest.zip
-rm commandlinetools-linux-8512546_latest.zip
+# curl -LO https://dl.google.com/android/repository/commandlinetools-linux-8512546_latest.zip
+# unzip commandlinetools-linux-8512546_latest.zip
+# rm commandlinetools-linux-8512546_latest.zip
 cd cmdline-tools/bin
-yes | ./sdkmanager --sdk_root=$SDK_DIR "platform-tools" "platforms;android-28"
-yes | ./sdkmanager --sdk_root=$SDK_DIR --install emulator
-yes | ./sdkmanager --sdk_root=$SDK_DIR "system-images;android-28;default;x86_64"
-yes | ./sdkmanager --sdk_root=$SDK_DIR --licenses
-./avdmanager create avd -n my_android_emu -k "system-images;android-28;default;x86_64"
+# yes | ./sdkmanager --sdk_root=$SDK_DIR "platform-tools" "platforms;android-28"
+# yes | ./sdkmanager --sdk_root=$SDK_DIR --install emulator
+# yes | ./sdkmanager --sdk_root=$SDK_DIR "system-images;android-28;default;x86_64"
+# yes | ./sdkmanager --sdk_root=$SDK_DIR --licenses
+./avdmanager create avd --force -n my_android_emu -k "system-images;android-28;default;x86_64"
+cd ../..
+
+curl -LO https://dl.google.com/android/repository/android-ndk-r25b-linux.zip
+unzip android-ndk-r25b-linux.zip
+rm android-ndk-r25b-linux.zip
+ANDROID_NDK_DIR=$SCRIPT_DIR/android-ndk-r25b
